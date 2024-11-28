4da813cc47630af4c63ea6e7f74bb781587b0dd4
ezazee <yukiirima123@gmail.com>
2024-11-28 15:05:53 +0700

    sadasdasdsad

--- resources/views/frontend/components/top-nav.blade.php
+++ resources/views/frontend/components/top-nav.blade.php
@@ -1,10 +1,20 @@
 {{-- TopNav --}}
 <div id="top-nav" class="top-nav style-one">
     <div class="mx-auto">
-        <div class="top-nav-main flex justify-center items-center bg-cover">
+        <div class="top-nav-main justify-center items-center bg-cover">
           <a href="#">
             <img src="{{ asset('frontend/assets/images/Navbar_PE_1857_x_44.gif') }}" alt="PE Skin Pro" class="h-[26px] md:h-[44px] object-cover">
           </a>
         </div>
     </div>
 </div>
+
+<style>
+    .top-nav-main img {
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    object-position: center; /* Posisikan gambar jika ukuran tidak proporsional */
+}
+
+</style>
