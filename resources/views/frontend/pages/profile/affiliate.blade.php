05893b05941f6c68a9d27b6074e08e68125a02b2
Risqiyyyy <raisafabriani05@gmail.com>
2025-03-12 11:39:42 +0700

    adada

--- resources/views/frontend/pages/profile/affiliate.blade.php
+++ resources/views/frontend/pages/profile/affiliate.blade.php
@@ -52,7 +52,7 @@
                                             Produk</th>
                                         <th scope="col"
                                             class="pb-3 text-left text-sm font-bold uppercase text-secondary whitespace-nowrap">
-                                            Referral</th>
+                                            User Referral</th>
                                         <th scope="col"
                                             class="pb-3 text-left text-sm font-bold uppercase text-secondary whitespace-nowrap">
                                             Komisi</th>
@@ -71,7 +71,7 @@
                                             Produk Tidak Ditemukan
                                         @endif
                                         </td>
-                                        <td>{{ $item->referredUser->referral_code ?? 'Tidak Ada Referral' }}</td>
+                                        <td>{{ $item->referredUser->name ?? 'Tidak Ada Referral' }}</td>
                                         <td>Rp {{ number_format($item->affiliate->commission, 0, ',', '.') }}</td>
                                         <td class="py-3 text-right">{{ $item->created_at->format('d M Y') }}</td>
                                     </tr>
